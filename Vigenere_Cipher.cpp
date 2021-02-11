66927ed41fe3a54c6080e6747a1ab7579c274d83
Divyam Verma <60467333+pokeychip19946@users.noreply.github.com>
2021-02-12 01:05:01 +0530

    Add files via upload

---
+++ Vigenere_Cipher.cpp
@@ -0,0 +1,69 @@
+#include<iostream>
+#include<stdio.h>
+#include<string.h>
+#include<stdlib.h>
+
+#ifndef ONLINE_JUDGE
+#define freopen freopen("input.txt","r",stdin);freopen("output.txt","w",stdout);
+#else
+#define freopen //comment
+#endif
+
+using namespace std;
+
+
+int main()
+{
+  freopen
+  string plaintext, ciphertext, key, output;
+  bool choice;
+  
+  cout<<"enter the plaintext"<<endl;
+  cin>>plaintext;
+  cout<<"enter the key"<<endl;
+  cin>>key;
+
+  string newkey = key;
+
+  while(newkey.length()<=plaintext.length())
+  {
+    newkey+=key;
+
+  }
+
+  cout<<"enter the choice either 1 or 0"<<endl;
+  cin>>choice;
+
+  if(choice==1)
+  {
+    for(int i = 0; i < plaintext.length(); i++)
+    {
+      ciphertext +=(plaintext[i]+newkey[i])%26 +65;
+
+    }
+
+    cout<<"ciphertext:"<<ciphertext<<endl;
+  }
+
+  else
+  {
+    for(int i=0; i < plaintext.length(); i++)
+    {
+      output+=(plaintext[i]-newkey[i]+26)%26 + 65;
+    }
+
+    cout<<"output:"<<output<<endl;
+  }
+
+  /*for(int i = 0; i < plaintext.length(); i++)
+  {
+    ciphertext +=(plaintext[i]-newkey[i]+26)%26 +65;
+
+  }
+
+  cout<<"ciphertext:"<<ciphertext<<endl;*/
+
+  return 0;
+
+
+}
